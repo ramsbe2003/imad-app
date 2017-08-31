9e9034fd816fadc7604ce077610809520342eddb
ramsbe2003 <31036129+ramsbe2003@users.noreply.github.com>
2017-09-01 00:00:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-    res.send('Article one is served')
+app.get('/ui/jarivs', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'jarvis.html'))
 })
 
 app.get('/ui/style.css', function (req, res) {
